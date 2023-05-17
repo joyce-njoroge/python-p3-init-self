2ffca4b5c9b61aabcfd496caf31e6c438eb10f09
Joyce Njoroge <joycewambuku33@gmail.com>
2023-05-17 11:30:47 +0300

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed='Mutt'):
+        self.name = name
+        self.breed = breed
+
+my_dog = Dog('Max', 'Labrador')
+print(my_dog.name)   
+print(my_dog.breed) 
+
+another_dog = Dog('Buddy')
+print(another_dog.name)   
+print(another_dog.breed)     
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+person1 = Person("Alice")
+print(person1.name)  
+
+person2 = Person("Bob")
+print(person2.name)  
